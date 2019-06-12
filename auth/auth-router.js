267b0900ca57e93d76d2216876da19795afec4fe
Kayla <6lackmacaroon@gmail.com>
2019-06-12 14:09:28 -0600

    built login route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,6 +8,7 @@ const secrets = require('../config/secrets.js');
 // endpoints /api/auth
 router.post('/register', (req, res) => {
       let user = req.body;
+      // hash the password
       const hash = bcrypt.hashSync(user.password, 12); // 2^n
       user.password = hash;
 
@@ -19,9 +20,30 @@ router.post('/register', (req, res) => {
                   console.log('register', err)
                   res.status(500).json({ message: 'registration failure' })
             })
-})
+});
 
+router.post('/login', (req, res) => {
+      let { username, password } = req.body;
 
+      Users.findBy({ username })
+            .first()
+            .then(user => {
+                  if(user && bcrypt.compareSync(password, user.password)) {
+                        // generate a token
+                        const token = generateToken(user)
+                        res.status(200).json({
+                              message: `Hello ${user.username}.`,
+                              token
+                        });
+                  } else {
+                        res.status(401).json({ message: 'invalid login credentials'})
+                  }
+            })
+            .catch(err => {
+                  console.log('login', err)
+                  res.status(500).json({ message: 'log in failure' })
+            })
+});
 
 
 
